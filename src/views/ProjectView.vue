1f9bcb8d4333f8e590f09edcbe00df714d8962aa
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-01-09 16:02:10 +0200

    minor test updates

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,12 +1,40 @@
 <script setup>
   import { useRoute } from 'vue-router'
   import { useStore } from '../store/index';
+  import { supabase } from '../supabase'
 
   const store = useStore()
   const route = useRoute()
 
   const group = route.params.group
   const project = route.params.project
+
+  async function getProjectDetails() {  // still busy building and testing
+    
+    try {
+      loading.value = true
+
+      let { data, error, status } = await supabase
+        .from('projects')
+        .select(`project_id, wallet, group_id`)
+        .eq('project_name', store.project)
+        
+      if (error && status !== 406) throw error
+
+      if (data) {
+        for (let j in data) {
+          console.log("loading", j)
+          groupid.value.push(data[j].group_id)
+          group_updated_at.value.push(data[j].updated_at)
+          groupname.value.push(data[j].group_name)
+        }
+      }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      loading.value = false
+    }
+  }
   
 </script>
 
--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -1,7 +1,36 @@
 <script setup>
+import { ref } from 'vue'
+import { useGetWallet } from '../composables/getwallet'
+
+const inputValue = ref('')
+const balance = ref('')
+const picture = ref('')
+const fTokens = ref('')
+const fTokenAmounts = ref('')
+const loadWallet = ref(false)
+
+function updateValue(event) {
+  inputValue.value = event.target.value
+}
+
+async function submitValue() {
+  loadWallet.value = true
+  const { lovelace, tokens, tokenAmounts, nfts, nftPicture } = await useGetWallet(inputValue.value)
+  balance.value = lovelace.value
+  console.log(tokens.value,tokenAmounts.value,nfts.value,nftPicture.value)
+  picture.value = nftPicture.value
+  fTokens.value = tokens.value
+  fTokenAmounts.value = tokenAmounts.value
+}
+
 </script>
+
 <template>
     <div>
-        Q4 2023
+      <input type="text" v-model="inputValue" @input="updateValue">
+      <button @click="submitValue">Submit</button>
+      <p>{{ balance }} ADA</p>
     </div>
 </template>
+
+<style></style>
